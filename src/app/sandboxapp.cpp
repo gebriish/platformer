5757683aff83c98d95517ac2df8c9dbb5c4de991
gebriish <gaureeshjr@gmail.com>
2024-09-28 18:44:26 +0530

    batch renderer first step

--- src/app/sandboxapp.cpp
+++ src/app/sandboxapp.cpp
@@ -15,12 +15,19 @@ namespace APP
 	WORLD::Entity e0(0);
 	WORLD::Entity e1(1);
 
+	WORLD::Entity *grid[256];
+
 	f32 AspectRatio = 1.6f;
 
 	f32 deltaTime = 0.0f;
 
 	void SandboxApp::Init()
 	{	
+
+		RENDERER::Init();
+
+		RENDERER::FlushEntityRenderQueue();
+
 		e0.SetPosition(-32, 0.0f);
 		e1.SetPosition(32, 0.0f);
 
@@ -29,11 +36,24 @@ namespace APP
 		
 		e0.SetColor(RENDERER::Color(204, 57, 47, 255));
 
-		RENDERER::Init();
+		for(int i=0; i<16; i++)
+		{
+			for(int j=0; j<16; j++)
+			{
+				WORLD::Entity *e = new WORLD::Entity(i * 16 + j + 2);
+				e->SetSize(10, 10);
+				e->SetPosition((i - 8) * 16 , (j - 8) * 16);
+				
+				RENDERER::Color c ((u8)((i / 15.0f) * 255), (u8)((j / 15.0f) * 255), 0, 255);
+				e->SetColor(c);
+				grid[i * 16 + j] = e;
+				RENDERER::QueueEntityRenderCall(*e);
+			}
+		}
 
-		RENDERER::FlushEntityRenderQueue();
 		RENDERER::QueueEntityRenderCall(e1);
 		RENDERER::QueueEntityRenderCall(e0);
+
 	}
 
 	void SandboxApp::Update(f32 dt)
@@ -92,6 +112,9 @@ namespace APP
 	void SandboxApp::Cleanup()
 	{
 		RENDERER::Cleanup();
+
+		for(WORLD::Entity* e : grid)
+			delete e;
 	}
 
 }
